ef19a7274e34a3c5c87f82c9fbb207abc9069304
Aldo Ziflaj <aldoziflaj95@gmail.com>
2017-05-08 13:41:45 +0200

    loading

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -19,6 +19,19 @@ class App extends Component {
     return (
       <HashRouter>
         <div className="html-content">
+          <div id="loading">
+            <div className="sk-cube-grid">
+              <div className="sk-cube sk-cube1"></div>
+              <div className="sk-cube sk-cube2"></div>
+              <div className="sk-cube sk-cube3"></div>
+              <div className="sk-cube sk-cube4"></div>
+              <div className="sk-cube sk-cube5"></div>
+              <div className="sk-cube sk-cube6"></div>
+              <div className="sk-cube sk-cube7"></div>
+              <div className="sk-cube sk-cube8"></div>
+              <div className="sk-cube sk-cube9"></div>
+            </div>
+          </div>
           <AppBar />
           <div className="container">
             <Route exact path="/" component={Login} />
