82848c886a7fa6174b67c9cd2ad3e67351760ae7
Sara Kola <sara.kolaa@hotmail.com>
2017-05-09 16:34:23 +0200

    me shume commits se aldo

---
+++ client/src/components/nav/AdminLinks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import '../../styles.css';
+
+class AdminLinks extends Component {
+  render() {
+    return (
+      <ul className="nav navbar-nav">
+        <Link to='/supporters'>All Supporters</Link>
+        <Link to='/tickets/all'>All Tickets</Link>
+      </ul>
+    );
+  }
+}
+export default AdminLinks;
