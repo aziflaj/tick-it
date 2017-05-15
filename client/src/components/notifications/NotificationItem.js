6e32ee20d0f910bf566b8ea892882c8d8bb345ce
Sara Kola <sara.kolaa@hotmail.com>
2017-05-15 23:19:28 +0200

    opening notifications

--- client/src/components/notifications/NotificationItem.js
+++ client/src/components/notifications/NotificationItem.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
 import baseUrl from '../../config/constants';
 
 class NotificationItem extends Component {
-  markAsRead() {
+  markAsRead(action) {
     axios({
       method: 'put',
       url: `${baseUrl}/notifications/${this.props.id}/read`,
       headers: { Authorization: `Bearer ${localStorage.getItem('token')}`}
     }).then((response) => {
-      if (response.data.status === 'ok') {
+      if (response.data.status === 'ok' && action === 'mark') {
         window.location.reload();
+      } else {
+        this.context.router.history.push(`/ticket/${this.props.ticket}`);
       }
     });
   }
@@ -19,16 +22,21 @@ class NotificationItem extends Component {
   render() {
     let mark = '';
     if(this.props.read === 'false') {
-      mark = (<button className="pull-right btn btn-primary" onClick={this.markAsRead.bind(this)}>
+      mark = (<button className="pull-right btn btn-primary" onClick={this.markAsRead.bind(this, "mark")}>
         Mark as read
       </button>);
     }
     return (
       <div className="notification well">
-        <p>{this.props.message}</p>
+        <p onClick={this.markAsRead.bind(this, "open")}>{this.props.message}</p>
         {mark}
       </div>
     );
   }
 }
+
+NotificationItem.contextTypes = {
+  router: PropTypes.object.isRequired
+}
+
 export default NotificationItem;
