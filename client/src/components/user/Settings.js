6a9fc53adbabc499c3f8f8c242c6fcc5249708cc
Sara Kola <sara.kolaa@hotmail.com>
2017-05-22 00:18:12 +0200

    todo: functionality

--- client/src/components/user/Settings.js
+++ client/src/components/user/Settings.js
@@ -15,6 +15,10 @@ class Settings extends Component {
       username: user.username,
       email: user.email,
       full_name: user.full_name,
+      password: user.password,
+      inputPassword: '',
+      newPassword: '',
+      confirmPassword: '',
       disabled: false,
       errorMessage: ''
     };
@@ -52,7 +56,7 @@ class Settings extends Component {
     this.context.router.history.push(`/users/${JSON.parse(localStorage.getItem('user')).username}`);
   }
 
-  onFormSubmit(e) {
+  onInfoFormSubmit(e) {
     const user = JSON.parse(localStorage.getItem('user'));
     const data = {
       username: this.state.username,
@@ -76,62 +80,146 @@ class Settings extends Component {
     });
   }
 
+  onCurrentPasswordChange(e) {
+    this.setState({ inputPassword: e.target.value });
+  }
+
+  onNewPasswordChange(e) {
+    this.setState({ newPassword: e.target.value });
+  }
+
+  onConfirmPasswordChange(e) {
+    this.setState({ confirmPassword: e.target.value });
+  }
+
+  onPassFormSubmit(e) {
+
+  }
+
   render() {
     return (
-      <form className="form-horizontal" onSubmit={this.onFormSubmit.bind(this)}>
-        <h2 className="form-signin-heading">Settings</h2>
-        <div className="form-group">
-          <label htmlFor="inputFullName" className="control-label col-sm-4">Full Name</label>
-          <div className="col-sm-8">
-            <input id="inputFullName"
-              type="text"
-              className="form-control"
-              value={this.state.full_name}
-              required
-              autoFocus
-              onChange={this.onFullNameChange.bind(this)}
-              />
-          </div>
-        </div>
+      <div className="row">
+        <ul className="nav nav-pills nav-stacked col-md-3" role="tablist">
+          <li role="presentation" className="active">
+            <a href="#general" aria-controls="unread" role="tab" data-toggle="tab">General</a>
+          </li>
+          <li role="presentation">
+            <a href="#password" aria-controls="all" role="tab" data-toggle="tab">Password</a>
+          </li>
+        </ul>
 
-        <div className="form-group">
-          <label htmlFor="inputUsername" className="control-label col-sm-4">Username</label>
-          <div className="col-sm-8">
-            <input id="inputUsername"
-              type="text"
-              className="form-control"
-              value={this.state.username}
-              required
-              autoFocus
-              onChange={this.onUsernameChange.bind(this)}
-              />
-          </div>
-        </div>
+        <div className="tab-content col-md-8">
+          <div role="tabpanel" className="tab-pane active" id="general">
+            <form className="form-horizontal" onSubmit={this.onInfoFormSubmit.bind(this)}>
+              <h4 className="form-signin-heading">Update your profile information</h4>
+              <br />
+              <div className="form-group">
+                <label htmlFor="inputFullName" className="control-label col-sm-4">Full Name</label>
+                <div className="col-sm-8">
+                  <input id="inputFullName"
+                    type="text"
+                    className="form-control"
+                    value={this.state.full_name}
+                    required
+                    autoFocus
+                    onChange={this.onFullNameChange.bind(this)}
+                    />
+                </div>
+              </div>
 
-        <div className="form-group">
-          <label htmlFor="inputEmail" className="control-label col-sm-4">E-Mail</label>
-          <div className="col-sm-8">
-            <input id="inputEmail"
-              type="text"
-              className="form-control"
-              value={this.state.email}
-              required
-              autoFocus
-              onChange={this.onEmailChange.bind(this)}
-              />
-          </div>
-        </div>
+              <div className="form-group">
+                <label htmlFor="inputUsername" className="control-label col-sm-4">Username</label>
+                <div className="col-sm-8">
+                  <input id="inputUsername"
+                    type="text"
+                    className="form-control"
+                    value={this.state.username}
+                    required
+                    autoFocus
+                    onChange={this.onUsernameChange.bind(this)}
+                    />
+                </div>
+              </div>
 
-        <div className="form-group">
-          <div className="pull-left col-sm-offset-2 col-sm-10">
-            <button className="btn btn-sm btn-danger" onClick={this.onDeleteAccount.bind(this)} disabled={this.state.disabled}>Delete Account</button>
+              <div className="form-group">
+                <label htmlFor="inputEmail" className="control-label col-sm-4">E-Mail</label>
+                <div className="col-sm-8">
+                  <input id="inputEmail"
+                    type="text"
+                    className="form-control"
+                    value={this.state.email}
+                    required
+                    autoFocus
+                    onChange={this.onEmailChange.bind(this)}
+                    />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="pull-left">
+                  <button className="btn btn-sm btn-danger" onClick={this.onDeleteAccount.bind(this)} disabled={this.state.disabled}>Delete Account</button>
+                </div>
+                <div className="pull-right">
+                  <button className="btn btn-sm btn-default" onClick={this.onCanceledRequest.bind(this)} disabled={this.state.disabled}>Cancel</button>
+                  <button className="btn btn-sm btn-primary" type="submit" disabled={this.state.disabled}>Save</button>
+                </div>
+              </div>
+            </form>
           </div>
-          <div className="pull-right">
-            <button className="btn btn-sm btn-default" onClick={this.onCanceledRequest.bind(this)} disabled={this.state.disabled}>Cancel</button>
-            <button className="btn btn-sm btn-primary" type="submit" disabled={this.state.disabled}>Save</button>
+          <div role="tabpanel" className="tab-pane" id="password">
+            <form className="form-horizontal" onSubmit={this.onPassFormSubmit.bind(this)}>
+              <h4 className="form-signin-heading">Change your password</h4>
+              <br />
+              <div className="form-group">
+                <label htmlFor="inputCurrentPassword" className="control-label col-sm-4">Current Password</label>
+                <div className="col-sm-8">
+                  <input id="inputCurrentPassword"
+                    type="password"
+                    className="form-control"
+                    required
+                    autoFocus
+                    onChange={this.onCurrentPasswordChange.bind(this)}
+                    />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="inputNewPassword" className="control-label col-sm-4">New Password</label>
+                <div className="col-sm-8">
+                  <input id="inputNewPassword"
+                    type="password"
+                    className="form-control"
+                    required
+                    autoFocus
+                    onChange={this.onNewPasswordChange.bind(this)}
+                    />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="inputConfirmPassword" className="control-label col-sm-4">Confirm Password</label>
+                <div className="col-sm-8">
+                  <input id="inputConfirmPassword"
+                    type="password"
+                    className="form-control has-feedback"
+                    required
+                    autoFocus
+                    onChange={this.onConfirmPasswordChange.bind(this)}
+                    />
+                </div>
+              </div>
+
+              <div className="form-group">
+                <div className="pull-left">
+                  <button className="btn btn-sm btn-default" onClick={this.onCanceledRequest.bind(this)} disabled={this.state.disabled}>Cancel</button>
+                </div>
+                <div className="pull-right">
+                  <button className="btn btn-sm btn-primary" type="submit" disabled={this.state.disabled}>Save</button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-      </form>
+      </div>
     );
   }
 }
